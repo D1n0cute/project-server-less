a23da580b27f82f9072efb828c39ff64c974b968
Pannatron47kanla <pannatronkanla@gmail.com>
2026-05-07 00:39:31 +0700

    fix frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/frontend/",   // 👈 เพิ่มบรรทัดนี้
   plugins: [react()],
 
   test: {
