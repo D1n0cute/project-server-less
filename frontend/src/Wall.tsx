64cb77f2b4c3df87096c34662a9990c7aa2e471b
Pannatron47kanla <pannatronkanla@gmail.com>
2026-05-16 09:13:13 +0700

    test 2 way

--- frontend/src/Wall.tsx
+++ frontend/src/Wall.tsx
@@ -167,7 +167,7 @@ export default function Wall() {
         <div className="card">
           <h2 className="card-title">ส่งกำลังใจให้โลก</h2>
           <p className="card-sub">
-            พิมพ์ข้อความ แล้วมันจะลอยอยู่บนกำแพง :3
+            พิมพ์ข้อความ แล้วมันจะลอยอยู่บนกำแพง :a
           </p>
 
           <textarea
